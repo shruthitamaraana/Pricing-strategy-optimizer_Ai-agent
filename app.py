8ee50f96b8ab552246439d3f33ee42a048ae54f6
shruthitamaraana <shruthitamaraana721@gmail.com>
2025-03-16 15:06:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -351,10 +351,10 @@ def main():
         """)
     
         # Display an Image
-        st.image(
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpu_ysYHqqhb4-8Ows_oZPCGSabJVBMrAOog&s",
-            caption="Optimize Your Pricing Strategy",
-            use_container_width=True
+    st.image(
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpu_ysYHqqhb4-8Ows_oZPCGSabJVBMrAOog&s",
+          caption="Optimize Your Pricing Strategy",
+          use_container_width=True
         )
 
     # Sidebar for input methods
